77041d5f6a842cd31a0e96844334e28eb82219dd
Benjamin JALON <bjalon@qastia.com>
2023-01-12 14:55:05 +0100

    Add bootstrap

--- app.py
+++ app.py
@@ -3,19 +3,49 @@ from flask import render_template
 from flask_bootstrap import Bootstrap
 import pandas as pd
 from dash import Dash, dcc, html
+import dash_bootstrap_components as dbc
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kjhfdkjhgjkdfhgkjdfhg'
 bootstrap = Bootstrap(app)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
-dash_app = Dash(__name__, server=app, url_base_pathname='/dash/')
+dash_app = Dash(
+    __name__,
+    server=app,
+    url_base_pathname='/dash/',
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 
+soccer = pd.read_csv('data/fifa_soccer_players.csv')
+
+LOGO = "https://gretaformation.ac-orleans-tours.fr/sites/all/themes/themes/adscom/images/logo.jpg"
+navbar = dbc.Navbar(
+    color="dark", dark=True,
+    children=[dbc.Container([
+        html.A(
+            href="/", style={"textDecoration": "none"},
+            children=dbc.Row(
+                align="center", className="g-0",
+                children=[
+                    dbc.Col(html.Img(src=LOGO, height="36px")),
+                ]),
+
+        ),
+        html.Div(html.Ul([
+            html.Li([
+                dbc.NavItem(dbc.NavLink("Home", href="/", external_link=True, style={'color': 'white'}))
+            ])
+        ], className="nav navbar-nav")
+            , className="navbar-collapse collapse")
+    ])])
 
 dash_app.layout = html.Div(children=[
+    navbar,
     html.H1(children='Hello Dash'),
     html.Div(children='''
         Dash: A web application framework for your data.
     '''),
+
 ])
 
 
